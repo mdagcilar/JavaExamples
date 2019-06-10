5e03246d6cc98fce855a00e4d6c7b37e1671d797
metin.dagcilar <metin.dagcilar@ocado.com>
2019-06-10 13:29:09 +0100

    Adding Stack implementation with assertJ and junit 5

---
+++ src/test/java/com/development/metindagcilar/stack/MyStackTest.java
@@ -0,0 +1,33 @@
+package com.development.metindagcilar.stack;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MyStackTest {
+
+    MyStack stack;
+
+    @BeforeEach
+    private void setup() {
+        stack = new MyStack();
+    }
+
+    @Test
+    void pop() {
+        stack.push("A");
+        assertThat(stack.pop()).isEqualTo("A");
+    }
+
+    @Test
+    void peek() {
+        stack.push("A");
+        assertThat(stack.peek()).isEqualTo("A");
+    }
+
+    @Test
+    void isEmpty() {
+        assertThat(stack.isEmpty()).isTrue();
+    }
+}
